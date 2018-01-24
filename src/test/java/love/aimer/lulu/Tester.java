37e8526a0cd866e041a47765aecbefe2cf417040
王佳 <wangjia_1919@163.com>
2018-01-24 13:44:08 +0800

    修改异常处理bug

--- src/test/java/love/aimer/lulu/Tester.java
+++ src/test/java/love/aimer/lulu/Tester.java
@@ -1,8 +1,15 @@
 package love.aimer.lulu;
 
+import love.aimer.lulu.datasource.SimpleDataSource;
+import org.junit.Test;
+
 /**
  * @author wj42134
  * @date 2018/1/24 00:07
  */
 public class Tester {
+    @Test
+    public void testProps() {
+        SimpleDataSource simpleDataSource = new SimpleDataSource(null);
+    }
 }
